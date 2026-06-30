b5d1ad355706db616ef8342c000d5f2536313983
unovo-ru <uli.nr.97@gmail.com>
2026-06-30 14:37:32 +0200

    hola

--- cpp04/src/Animal.cpp
+++ cpp04/src/Animal.cpp
@@ -1,28 +1,42 @@
 #include "Animal.hpp"
 
-Animal::Animal(){}
+using std::cout; using std::endl;
 
-Animal::~Animal(){}
+Animal::Animal() : _type("")
+{
+	cout	<< "Animal Default contructor called"
+			<< endl;
+}
+
+Animal::~Animal()
+{
+	cout	<< "Animal destructor called"
+			<< endl;
+}
 
 Animal::Animal(std::string const &type) : _type(type)
 {
-
+	cout	<< "Animal constructor called"
+			<< endl;
 }
-Animal::Animal(Animal const &src)
+Animal::Animal(Animal const &src) : _type(src._type)
 {
-
+	cout	<< "Animal copy constructor called"
+			<< endl;
 }
 Animal& Animal::operator=(Animal const &src)
 {
-
+	if (this != &src)
+		_type = src._type;
+	return (*this);
 }
-
 
 std::string	Animal::getType() const
 {
-
+	return (_type);
 }
 void	Animal::makeSound() const
 {
-
+	cout	<< "Some generic animal sound"
+			<< endl;
 }
