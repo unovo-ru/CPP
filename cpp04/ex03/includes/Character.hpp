bb711843d2327934d18cdb9491a248e433131f4a
unovo-ru <uli.nr.97@gmail.com>
2026-07-03 22:49:45 +0200

    ex03 haciendo librerias de clases

--- cpp04/ex03/includes/Character.hpp
+++ cpp04/ex03/includes/Character.hpp
@@ -9,8 +9,8 @@ class Character : public ICharacter
 {
 	private:
 
-		std::string _name;
-		AMateria* _inventory[4]; // Aquí es donde vive el inventario de 4 slots
+		std::string		_name;
+		AMateria		*_inventory[4]; // Aquí es donde vive el inventario de 4 slots
 
 
 	public:
@@ -25,10 +25,17 @@ class Character : public ICharacter
 		Character &operator=(const Character &src);
 
 		// Implementación de los métodos virtuales puros de la interfaz
-		virtual std::string const & getName() const;
-		virtual void equip(AMateria* m);
-		virtual void unequip(int idx);
-		virtual void use(int idx, ICharacter& target);
+		virtual std::string const	&getName() const;
+		virtual void				equip(AMateria* m);
+		virtual void				unequip(int idx);
+		virtual void				use(int idx, ICharacter& target);
 };
+
+/*1. El mundo de las Materias (Habilidades)
+
+	AMateria (La plantilla base): Es el concepto abstracto de "magia" o "energía". No puedes crear una AMateria a secas porque "la magia genérica" no existe en el juego; la magia tiene que ser de algo concreto.  
+
+	Ice y Cure (Las habilidades reales): Estas son las magias concretas que el jugador puede lanzar. Son clases instanciables (puedes hacer new Ice() o new Cure()). Al heredar de AMateria, ambas comparten la estructura de tener un tipo y la capacidad de ser usadas.  */
+
 
 #endif
