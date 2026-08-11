cc784e63d28bac569997828c7d9c4c01c4d21f01
unovo-ru <uli.nr.97@gmail.com>
2026-08-11 08:55:53 +0200

    comienzo cpp06 ex02

--- cpp06/ex02 copy/include/Base.hpp
+++ cpp06/ex02 copy/include/Base.hpp
@@ -4,7 +4,6 @@
 class A;
 class B;
 class C;
-
 class Base {
 	public:
 		virtual	~Base(){};
---
+++ cpp06/ex02 copy/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "../include/TerminalStyles.h"
+#include "../include/Base.hpp"
+#include "../include/A.hpp"
+#include "../include/B.hpp"
+#include "../include/C.hpp"
+
+int main(void) {
+    std::cout << GREEN << "Proyecto compilado con éxito." << RST << std::endl;
+    return 0;
+}
