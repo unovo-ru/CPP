0654d27221162d7190ee67a678afee64e2177313
unovo-ru <uli.nr.97@gmail.com>
2026-09-15 13:15:23 +0200

    cpp08 ex01 terminado y revisado

--- cpp08/ex01/includes/Span.hpp
+++ cpp08/ex01/includes/Span.hpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <list>
 #include <set>
 
 
--- cpp08/ex01/src/main.cpp
+++ cpp08/ex01/src/main.cpp
@@ -4,6 +4,7 @@ int	main()
 {
 	/*SUBJECT EXAMPLE:*/
 	{
+		std::cout	<< "---STANDAR SUBJECT TEST---\n";
 		Span sp = Span(5);
 		sp.addNumber(6);
 		sp.addNumber(3);
@@ -16,36 +17,73 @@ int	main()
 
 	/*RANGE EXAMPLES*/
 	{
-		Span sp = Span(10000);
+		/*VECTOR*/
+		{
+			std::cout	<< "---VECTOR---\n";
+			Span sp = Span(10000);
+			/*CORRECT*/
+			try
+			{
+				std::vector<int>	nums;
+				for (int i = 0; i < 10000; i++)
+					nums.push_back(i);
+				sp.addRange(nums.begin(), nums.end());
+				std::cout	<< sp.shortestSpan() << std::endl
+							<< sp.longestSpan() << std::endl;
+			}
+			catch(const std::exception& e)
+			{
+				std::cerr << e.what() << '\n';
+			}
 
-		/*CORRECT*/
-		try
-		{
-			std::vector<int>	nums;
-			for (int i = 0; i < 10000; i++)
-				nums.push_back(i);
-			sp.addRange(nums.begin(), nums.end());
-			std::cout	<< sp.shortestSpan() << std::endl
-						<< sp.longestSpan() << std::endl;
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << e.what() << '\n';
+			/*INCORRECT (THE CONTAINER IS SMALLER THAN THE RANGE)*/
+			try
+			{
+				std::vector<int>	nums;
+				for (int i = 0; i < 19999; i++)
+					nums.push_back(i);
+				sp.addRange(nums.begin(), nums.end());
+				std::cout	<< sp.shortestSpan() << std::endl
+							<< sp.longestSpan() << std::endl;
+			}
+			catch(const std::exception& e)
+			{
+				std::cerr << e.what() << '\n';
+			}
 		}
 
-		/*INCORRECT (THE CONTAINER IS SMALLER THAN THE RANGE)*/
-		try
+		/*LIST*/
 		{
-			std::vector<int>	nums;
-			for (int i = 0; i < 19999; i++)
-				nums.push_back(i);
-			sp.addRange(nums.begin(), nums.end());
-			std::cout	<< sp.shortestSpan() << std::endl
-						<< sp.longestSpan() << std::endl;
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << e.what() << '\n';
+			std::cout	<< "---LIST---\n";
+			Span sp = Span(10000);
+			try
+			{
+				std::list<int>	nums;
+				for (int i = 0; i < 10000; i++)
+					nums.push_back(i);
+				sp.addRange(nums.begin(), nums.end());
+				std::cout	<< sp.shortestSpan() << std::endl
+							<< sp.longestSpan() << std::endl;
+			}
+			catch(const std::exception& e)
+			{
+				std::cerr << e.what() << '\n';
+			}
+
+			/*INCORRECT (THE CONTAINER IS SMALLER THAN THE RANGE)*/
+			try
+			{
+				std::list<int>	nums;
+				for (int i = 0; i < 19999; i++)
+					nums.push_back(i);
+				sp.addRange(nums.begin(), nums.end());
+				std::cout	<< sp.shortestSpan() << std::endl
+							<< sp.longestSpan() << std::endl;
+			}
+			catch(const std::exception& e)
+			{
+				std::cerr << e.what() << '\n';
+			}
 		}
 	}
 	return (0);
